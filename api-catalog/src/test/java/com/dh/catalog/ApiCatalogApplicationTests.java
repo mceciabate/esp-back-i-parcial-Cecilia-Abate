6660c901a9f3a66e18908cfad0f3b1b8427fdedc
mceciabate <m.cecilia.abate@gmail.com>
2022-11-10 23:08:24 -0300

    testeo mock para path de catalogo

--- api-catalog/src/test/java/com/dh/catalog/ApiCatalogApplicationTests.java
+++ api-catalog/src/test/java/com/dh/catalog/ApiCatalogApplicationTests.java
@@ -1,13 +1,39 @@
 package com.dh.catalog;
 
-import org.junit.jupiter.api.Test;
+import com.dh.catalog.client.MovieServiceClient;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.runner.RunWith;
+import org.springframework.test.web.client.match.MockRestRequestMatchers;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+import java.util.ArrayList;
+
+import static org.junit.matchers.JUnitMatchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-class ApiCatalogApplicationTests {
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc(addFilters = false)
+public class ApiCatalogApplicationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
 
     @Test
-    void contextLoads() {
+    public void consultarPeliculasPorGenero() throws Exception {
+        ResultActions response;
+        response = this.mockMvc
+                .perform(get("/api/v1/catalog/{genre}", "Terror"))
+                .andDo(print()).andExpect(status().isOk());
     }
-
 }
