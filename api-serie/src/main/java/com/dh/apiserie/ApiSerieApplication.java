9550c366685444be27f34242e6d9f0fd7ede5d2b
mceciabate <m.cecilia.abate@gmail.com>
2022-12-10 13:45:58 -0300

    series

---
+++ api-serie/src/main/java/com/dh/apiserie/ApiSerieApplication.java
@@ -0,0 +1,13 @@
+package com.dh.apiserie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSerieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSerieApplication.class, args);
+	}
+
+}
