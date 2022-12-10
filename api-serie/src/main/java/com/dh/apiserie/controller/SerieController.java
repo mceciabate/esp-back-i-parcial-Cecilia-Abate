905f8b71a592e4db028cd395c2599eeb4e5c115d
mceciabate <m.cecilia.abate@gmail.com>
2022-12-10 15:24:50 -0300

    SerieController

---
+++ api-serie/src/main/java/com/dh/apiserie/controller/SerieController.java
@@ -0,0 +1,29 @@
+package com.dh.apiserie.controller;
+
+import com.dh.apiserie.model.SerieEntity;
+import com.dh.apiserie.service.SerieService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/series")
+public class SerieController {
+
+    private final SerieService serieService;
+
+    public SerieController(SerieService serieService) {
+        this.serieService = serieService;
+    }
+
+    @GetMapping("/{genre}")
+    ResponseEntity<List<SerieEntity>> getSeriesByGenre(@PathVariable String genre){
+        return ResponseEntity.ok().body(serieService.getByGenre(genre));
+    }
+
+    @PostMapping("/save")
+    ResponseEntity<SerieEntity> saveSerie(@RequestBody SerieEntity serie){
+        return ResponseEntity.ok().body(serieService.save(serie);
+    }
+}
