7e4614541bb468995b67842f17fc5f40817391af
mceciabate <m.cecilia.abate@gmail.com>
2022-12-10 16:14:29 -0300

    config series

--- api-serie/src/main/java/com/dh/apiserie/ApiSerieApplication.java
+++ api-serie/src/main/java/com/dh/apiserie/ApiSerieApplication.java
@@ -2,8 +2,12 @@ package com.dh.apiserie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableMongoRepositories
 public class ApiSerieApplication {
 
 	public static void main(String[] args) {
