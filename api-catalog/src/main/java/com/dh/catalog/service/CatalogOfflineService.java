40b351a0e7b76981cfef8e7ce3d91ef371434026
mceciabate <m.cecilia.abate@gmail.com>
2022-12-11 19:11:54 -0300

    service consumer serie

--- api-catalog/src/main/java/com/dh/catalog/service/CatalogOfflineService.java
+++ api-catalog/src/main/java/com/dh/catalog/service/CatalogOfflineService.java
@@ -1,7 +1,9 @@
 package com.dh.catalog.service;
 
 import com.dh.catalog.model.MovieEntity;
+import com.dh.catalog.model.SerieEntity;
 import com.dh.catalog.repository.MovieMongoRepository;
+import com.dh.catalog.repository.SerieMongoRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -12,20 +14,35 @@ public class CatalogOfflineService {
 
     private final MovieMongoRepository movieMongoRepository;
 
+    private final SerieMongoRepository serieMongoRepository;
 
-    public CatalogOfflineService(MovieMongoRepository movieMongoRepository) {
+
+    public CatalogOfflineService(MovieMongoRepository movieMongoRepository, SerieMongoRepository serieMongoRepository) {
         this.movieMongoRepository = movieMongoRepository;
+        this.serieMongoRepository = serieMongoRepository;
     }
 
-    public List<MovieEntity> findMovieByGenre(String genre){
+    public List<MovieEntity> findMovieByGenre(String genre) {
         List<MovieEntity> allMovies = movieMongoRepository.findAll();
         List<MovieEntity> genreMovies = new ArrayList<>();
-        for (MovieEntity allMovie : allMovies) {
-            if (allMovie.getGenre().equals(genre)){
-                genreMovies.add(allMovie);
+        for (MovieEntity movie : allMovies) {
+            if (movie.getGenre().equals(genre)) {
+                genreMovies.add(movie);
             }
-        }return genreMovies;
+        }
+        return genreMovies;
     }
 
+    public List<SerieEntity> findSerieByGenre(String genre) {
+        List<SerieEntity> allSeries = serieMongoRepository.findAll();
+        List<SerieEntity> genreSeries = new ArrayList<>();
+        for (SerieEntity serie : allSeries) {
+            if (serie.getGenre().equals(genre)) {
+                genreSeries.add(serie);
+            }
+        }
+        return genreSeries;
 
+
+    }
 }
