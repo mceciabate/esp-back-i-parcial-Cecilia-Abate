b19979b223ba8a5b3852f60acd58077eae77c48d
mceciabate <m.cecilia.abate@gmail.com>
2022-12-11 21:26:31 -0300

    logs Movie Service

--- api-movie/src/main/java/com/dh/movie/service/MovieService.java
+++ api-movie/src/main/java/com/dh/movie/service/MovieService.java
@@ -4,6 +4,7 @@ package com.dh.movie.service;
 import com.dh.movie.event.NewMovieEventProducer;
 import com.dh.movie.model.Movie;
 import com.dh.movie.repository.MovieRepository;
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,7 +13,7 @@ import java.util.Optional;
 @Service
 public class MovieService {
 
-
+    final static Logger log = Logger.getLogger(MovieService.class);
     private final MovieRepository movieRepository;
     private final NewMovieEventProducer newMovieEventProducer;
 
@@ -22,28 +23,34 @@ public class MovieService {
     }
 
     public List<Movie> getAllMovies(){
+        log.info("Listando todas las peliculas");
         return movieRepository.findAll();
     }
 
     public Movie getMovieById(Long id) {
+        log.info("Buscando la pelicula numero " + id);
         return movieRepository.findById(id).orElse(null);
     }
 
     public List<Movie> findByGenre(String genre) {
+        log.info("Listando las peliculas del género "+ genre);
         return movieRepository.findByGenre(genre);
     }
 
     public Movie save(Movie movie) {
+        log.info("Guardando la película " + movie.getName());
         movieRepository.save(movie);
         newMovieEventProducer.execute(movie);
         return movie;
 
     }
     public void deleteMovie(Long id){
+        log.info("Eiminando la película con el id " + id);
         movieRepository.deleteById(id);
     }
 
     public void updateMovie(Movie movie){
+        log.info("Actualizando la película número" + movie.getId());
         if(movieRepository.existsById(movie.getId())){
             movieRepository.save(movie);
         }
